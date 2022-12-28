fb922126885a95234bb614cfb282471392155643
Artur Błażejczyk <arturb947@gmail.com>
2022-12-28 18:48:45 +0100

    bonding number button with logic

--- Calculator.WpfApp/ViewModels/MainViewModel.cs
+++ Calculator.WpfApp/ViewModels/MainViewModel.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Calculator.WpfApp.Commands;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Calculator.WpfApp.ViewModels
 {
@@ -13,9 +16,17 @@ namespace Calculator.WpfApp.ViewModels
         public MainViewModel() 
         {
             ScreenVal = "0";
+            AddNumberCommand = new RelayCommand(AddNumber);
+        }
+
+        private void AddNumber(object obj)
+        {
+            MessageBox.Show("Test");
         }
 
         private string _screenVal;
+
+        public ICommand AddNumberCommand { get; set; }
 
         public string ScreenVal
         {
