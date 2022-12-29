14e4f4307b3eb22ab3209490794fd5a058599e44
Artur Błażejczyk <arturb947@gmail.com>
2022-12-29 19:14:22 +0100

    add logic for operators and result button

--- Calculator.WpfApp/ViewModels/MainViewModel.cs
+++ Calculator.WpfApp/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -13,32 +14,65 @@ namespace Calculator.WpfApp.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        private string _screenVal;
+        private List<string> _availableOperations = new List<string> { "+", "-", "x", "/", "%"};
+        private DataTable _dataTable = new DataTable();
+        private bool _isLastSignAnOperation;
+
         public MainViewModel() 
         {
-            ScreenVal = "1";
+            ScreenVal = "0";
             AddNumberCommand = new RelayCommand(AddNumber);
-            AddOperationCommand = new RelayCommand(AddOperation);
+            AddOperationCommand = new RelayCommand(AddOperation, CanAddOperation);
+            ClearScreenCommand = new RelayCommand(ClearScreen);
+            GetResultCommand = new RelayCommand(GetResult, CanGetResult);
+        }
+
+        private bool CanGetResult(object obj) => !_isLastSignAnOperation;
+
+        private bool CanAddOperation(object obj) => !_isLastSignAnOperation;
+
+        private void GetResult(object obj)
+        {
+            var result = Math.Round(Convert.ToDouble(_dataTable.Compute(ScreenVal.Replace(",", "."), "")), 2);
+
+            ScreenVal = result.ToString();
+        }
+
+        private void ClearScreen(object obj)
+        {
+            ScreenVal= "0";
+            _isLastSignAnOperation = false;
         }
 
         private void AddOperation(object obj)
         {
-            ScreenVal += obj;
+            var operation = obj as string;
+
+            if(!_isLastSignAnOperation)
+            ScreenVal += operation;
+
+            _isLastSignAnOperation = true;
         }
 
         private void AddNumber(object obj)
         {
-            if (ScreenVal == "0")
-                ScreenVal = (string)obj;
-            else
-                ScreenVal += obj;
-        }
+            var number = obj as string;
 
-        private string _screenVal;
+            if (ScreenVal == "0" && number != ",")
+                ScreenVal = string.Empty;
+            else if (number== "," && _availableOperations.Contains(ScreenVal.Substring(ScreenVal.Length - 1)))
+                number = "0,";
+
+            ScreenVal += number;
+
+            _isLastSignAnOperation = false;
+        }
 
         public ICommand AddNumberCommand { get; set; }
         public ICommand AddOperationCommand { get; set; }
-
-
+        public RelayCommand ClearScreenCommand { get; set; }
+        public RelayCommand GetResultCommand { get; }
 
         public string ScreenVal
         {
